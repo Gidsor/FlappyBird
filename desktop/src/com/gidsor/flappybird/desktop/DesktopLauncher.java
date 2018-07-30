034200256b1d6bc59658ba4ba1f8f6ec2b71b863
Vadim Denisov <gidsor@gmail.com>
2018-07-30 23:13:33 +0500

    refactoring

--- desktop/src/com/gidsor/flappybird/desktop/DesktopLauncher.java
+++ desktop/src/com/gidsor/flappybird/desktop/DesktopLauncher.java
@@ -5,8 +5,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gidsor.flappybird.FlappyBird;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new FlappyBird(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new FlappyBird(), config);
+    }
 }
