753b19b09bb3cb1c4570471fb879df28ff29c6b2
Vadim Denisov <gidsor@gmail.com>
2018-08-04 09:14:37 +0300

    collides

--- core/src/com/gidsor/flappybird/sprites/Bird.java
+++ core/src/com/gidsor/flappybird/sprites/Bird.java
@@ -1,21 +1,26 @@
 package com.gidsor.flappybird.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.gidsor.flappybird.FlappyBird;
 
 public class Bird {
     public static final int GRAVITY = -15;
-    private static final int MOVEMENT = 100;
+    public static final int MOVEMENT = 100;
+
     private Vector3 position;
     private Vector3 velosity;
 
     private Texture bird;
 
+    private Rectangle bounds;
+
     public Bird(int x, int y) {
         position = new Vector3(x, y, 0);
         velosity = new Vector3(0,0,0);
         bird = new Texture("bird.png");
+        bounds = new Rectangle(x, y, bird.getWidth(), bird.getHeight());
     }
 
     public void update(float dt) {
@@ -30,6 +35,7 @@ public class Bird {
         }
 
         velosity.scl(1 / dt);
+        bounds.setPosition(position.x, position.y);
     }
 
     public void jump() {
@@ -42,5 +48,9 @@ public class Bird {
 
     public Texture getBird() {
         return bird;
+    }
+
+    public Rectangle getBounds() {
+        return bounds;
     }
 }
